579d5ba3c0a7dc81299c9f9f23d287a94a33c9b0
vinpav <vinpav.workn@gmail.com>
2018-03-14 21:03:44 +0100

    First try

---
+++ gladosfilewatcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script watches for file creations in the specified folder and sends them by email
+
+echo $$ > gew.pid
+
+if [ "$1" == "start" ]; then
+inotifywait -m $2 -e create |
+    while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        echo "The file '$file' appeared in directory '$path' via '$action'" | mail -s "Glados watch event" $3 -A $path$file
+    done
+elif [ "$1" == "stop" ]; then
+	echo "Shutting down Glados watcher on directory '$path'"
+	kill $(<gew.pid)
+else
+	echo "Usage : ./gladosfilewatcher.sh start|stop folder_to_watch notification_email"
+fi
