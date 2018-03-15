6bdec2d6c49264fbaaa804608c80504e2f3e9fe0
vinpav <vinpav.workn@gmail.com>
2018-03-15 17:08:37 +0100

    Notifier email address check moved into the correct command

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gladoseventwatcher
+A simple script that watches for new files in a given directory, and sends them to a given email address.
--- gladosfilewatcher.sh
+++ gladosfilewatcher.sh
@@ -2,20 +2,21 @@
 # This script watches for file creations in the specified folder and sends them by email
 
 echo "Setting watcher on " $2
-if [ $# == 3 ]; then
-	echo "Gladosfilewatcher will notify " $3
-else
-	echo "Notifier email not set : logging to output file only"
-fi
 
 if [ "$1" == "start" ]; then
-inotifywait -m $2 -e create |
+	if [ $# == 3 ]; then
+		echo "Gladosfilewatcher will notify " $3
+	else
+		echo "Notifier email not set : logging to output file only"
+	fi
+
+	inotifywait -m $2 -e create |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-	if [ $# == 3 ]; then
-		echo "Sending mail to "$3
-        	echo "The file '$file' appeared in directory '$path' via '$action'" | mail -s "Glados watch event" $3 -A $path$file
-	fi
+				if [ $# == 3 ]; then
+					echo "Sending mail to "$3
+			        	echo "The file '$file' appeared in directory '$path' via '$action'" | mail -s "Glados watch event" $3 -A $path$file
+				fi
     done
 elif [ "$1" == "stop" ]; then
 	echo "Shutting down Glados watcher on directory '$path'"
