0b565948ba4fe9cc3366952c36d20243e38051eb
vinpav <vinpav.workn@gmail.com>
2018-03-15 10:52:09 +0100

    Notifier email is now optional

--- gladosfilewatcher.sh
+++ gladosfilewatcher.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # This script watches for file creations in the specified folder and sends them by email
 
+echo "Setting watcher on " $2
+if [ $# == 3 ]; then
+	echo "Gladosfilewatcher will notify " $3
+else
+	echo "Notifier email not set : logging to output file only"
+fi
+
 if [ "$1" == "start" ]; then
 inotifywait -m $2 -e create |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-        echo "The file '$file' appeared in directory '$path' via '$action'" | mail -s "Glados watch event" $3 -A $path$file
+	if [ $# == 3 ]; then
+		echo "Sending mail to "$3
+        	echo "The file '$file' appeared in directory '$path' via '$action'" | mail -s "Glados watch event" $3 -A $path$file
+	fi
     done
 elif [ "$1" == "stop" ]; then
 	echo "Shutting down Glados watcher on directory '$path'"
